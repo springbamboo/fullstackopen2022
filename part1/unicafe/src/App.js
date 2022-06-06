3a9eaa6ef28be3248fd1ed98c2a3b7883dcf2e37
springbamboo <wuchengzhong1125@gmail.com>
2022-06-06 18:59:40 +0900

    Ex 1.6

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,11 +1,39 @@
 import React, { useState } from 'react';
 
+const Button = ({ onClick, text }) => {
+    return <button onClick={onClick}>{text}</button>;
+};
+
 const App = () => {
     const [good, setGood] = useState(0);
     const [neutral, setNeutral] = useState(0);
     const [bad, setBad] = useState(0);
 
-    return <div>code here</div>;
+    const handleGoodClick = () => {
+        setGood(good + 1);
+    };
+
+    const handleNeutralClick = () => {
+        setNeutral(neutral + 1);
+    };
+
+    const handleBadClick = () => {
+        setBad(bad + 1);
+    };
+
+    // console.log(good, neutral, bad);
+    return (
+        <div>
+            <h1>give feedback</h1>
+            <Button onClick={handleGoodClick} text="good" />
+            <Button onClick={handleNeutralClick} text="Neutral" />
+            <Button onClick={handleBadClick} text="Bad" />
+            <h1>statistics</h1>
+            <div>good {good}</div>
+            <div>neutral {neutral}</div>
+            <div>bad {bad}</div>
+        </div>
+    );
 };
 
 export default App;
