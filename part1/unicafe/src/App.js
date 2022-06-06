8031b0285412ca21040eff70bb658d440cc8e76e
springbamboo <wuchengzhong1125@gmail.com>
2022-06-06 19:08:51 +0900

    Ex 1.8

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -4,6 +4,20 @@ const Button = ({ onClick, text }) => {
     return <button onClick={onClick}>{text}</button>;
 };
 
+const Statistics = ({ good, neutral, bad }) => {
+    return (
+        <div>
+            <h1>statistics</h1>
+            <div>good {good}</div>
+            <div>neutral {neutral}</div>
+            <div>bad {bad}</div>
+            <div>all {good + neutral + bad}</div>
+            <div>average {(good * 1 - bad) / (good + neutral + bad)}</div>
+            <div>positive {good / (good + neutral + bad)}</div>
+        </div>
+    );
+};
+
 const App = () => {
     const [good, setGood] = useState(0);
     const [neutral, setNeutral] = useState(0);
@@ -21,20 +35,13 @@ const App = () => {
         setBad(bad + 1);
     };
 
-    // console.log(good, neutral, bad);
     return (
         <div>
             <h1>give feedback</h1>
             <Button onClick={handleGoodClick} text="good" />
             <Button onClick={handleNeutralClick} text="Neutral" />
             <Button onClick={handleBadClick} text="Bad" />
-            <h1>statistics</h1>
-            <div>good {good}</div>
-            <div>neutral {neutral}</div>
-            <div>bad {bad}</div>
-            <div>all {good + neutral + bad}</div>
-            <div>average {(good * 1 - bad) / (good + neutral + bad)}</div>
-            <div>positive {good / (good + neutral + bad)}</div>
+            <Statistics good={good} neutral={neutral} bad={bad} />
         </div>
     );
 };
