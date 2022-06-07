662024658f324a685016d768eef61cc51982d3e2
springbamboo <wuchengzhong1125@gmail.com>
2022-06-07 23:25:45 +0900

    Ex 2.5

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,41 +1,6 @@
 import React from 'react';
+import Course from './components/Course';
 
-const Head = ({ name }) => {
-    return <div>{name}</div>;
-};
-
-const Part = ({ parts }) => {
-    return (
-        <div>
-            <div>
-                {parts.name} {parts.exercises}
-            </div>
-        </div>
-    );
-};
-
-const Content = ({ part }) => {
-    const sum = part.reduce((total, part) => total + part.exercises, 0);
-    return (
-        <div>
-            {part.map((e) => (
-                <Part key={e.id} parts={e} />
-            ))}
-            <div>total of {sum} exercises</div>
-        </div>
-    );
-};
-
-const Course = ({ course }) => {
-    return (
-        <div>
-            <h1>
-                <Head name={course.name} />
-            </h1>
-            <Content part={course.parts} />
-        </div>
-    );
-};
 const App = () => {
     const courses = [
         {
