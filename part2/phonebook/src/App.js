0ec852c54deaff743e525c37d3f0c9652ffa709b
springbamboo <wuchengzhong1125@gmail.com>
2022-06-17 16:36:30 +0900

    Ex 2.7

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,42 @@
+import { React, useState } from 'react';
+
+const App = () => {
+    const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
+    const [newName, setNewName] = useState('new name');
+
+    const handleInputNameChange = (e) => {
+        console.log(e.target.value);
+        setNewName(e.target.value);
+    };
+
+    const handleButtonClick = (e) => {
+        e.preventDefault();
+        setPersons(persons.concat({ name: newName }));
+    };
+    return (
+        <div>
+            <h2>Phonebook</h2>
+            <form>
+                <div>
+                    name:{' '}
+                    <input value={newName} onChange={handleInputNameChange} />
+                </div>
+                <div>
+                    <button type="submit" onClick={handleButtonClick}>
+                        add
+                    </button>
+                </div>
+            </form>
+            <h2>Numbers</h2>
+            <div>
+                <ul>
+                    {persons.map((ele) => (
+                        <li key={ele.name}>{ele.name}</li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default App;
