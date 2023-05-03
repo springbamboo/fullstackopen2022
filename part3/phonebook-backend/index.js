574ddadeee4e05c3c7fa23c833204bff4c2a8168
springbamboo <wuchengzhong1125@gmail.com>
2023-05-03 17:20:42 +0900

    Ex 3.7 & 3.8

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,7 +1,17 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
+morgan.token('body', function getId(req) {
+    return JSON.stringify(req.body);
+});
+
 app.use(express.json());
+app.use(
+    morgan(
+        ':method :url :status :res[content-length] - :response-time ms :body'
+    )
+);
 
 let phonebook = [
     {
