b36c5a99e1066b04529b3b1c87259db3b2033752
springbamboo <wuchengzhong1125@gmail.com>
2023-05-10 00:58:17 +0900

    backend deploy

--- index.js
+++ index.js
@@ -106,6 +106,6 @@ app.post('/api/persons', (request, response) => {
     response.json(phonebook);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT);
 console.log(`Server running on port ${PORT}`);
