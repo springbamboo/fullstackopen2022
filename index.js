12bca0d9a43a69dfbf4170080d8c7ac121ebe106
springbamboo <wuchengzhong1125@gmail.com>
2023-08-13 01:30:29 +0900

    Ex 3.15~3.17

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const cors = require('cors');
 const Person = require('./model/person');
 const app = express();
 
-app.use(express.static('build'));
 app.use(cors());
+app.use(express.static('build'));
 
 morgan.token('body', function getId(req) {
     return JSON.stringify(req.body);
@@ -24,14 +24,41 @@ app.get('/api/persons', (request, response) => {
     });
 });
 
-app.get('/api/persons/:id', (request, response) => {
-    Person.findById(request.params.id).then((person) => {
-        if (person) {
-            response.json(person);
-        } else {
-            response.status(404).end();
-        }
-    });
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
+        .then((person) => {
+            if (person) {
+                response.json(person);
+            } else {
+                response.status(404).end();
+            }
+        })
+        .catch((error) => {
+            next(error);
+        });
+});
+
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndRemove(request.params.id)
+        .then((result) => {
+            response.status(204).end();
+        })
+        .catch((error) => next(error));
+});
+
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body;
+
+    const person = {
+        name: body.name,
+        number: body.number,
+    };
+
+    Person.findByIdAndUpdate(request.params.id, person)
+        .then((updatedNote) => {
+            response.json(updatedNote);
+        })
+        .catch((error) => next(error));
 });
 
 app.post('/api/persons', (request, response) => {
@@ -64,6 +91,15 @@ app.post('/api/persons', (request, response) => {
         response.json(person);
     });
 });
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message);
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' });
+    }
+    next(error);
+};
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT);
